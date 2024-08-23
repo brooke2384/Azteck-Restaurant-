f68f6e003a4b9a4a06efd04de673e5e9a839dcf3
brooke2384 <132254128+brooke2384@users.noreply.github.com>
2024-08-23 13:12:17 +0300

    Initial commit for Azteck Restaurant project

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Choose your favorite dishes, select dine-in or delivery, and enjoy a seamless ordering experience. Fresh, delicious, and made just for you—order now and indulge!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Cart.css'
+const cart = () => {
+  return (
+    <div>cart</div>
+  )
+}
+
+export default cart
