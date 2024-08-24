d59806c4fce8f24e7a72e97faee2f18ba6e824fa
brooke2384 <132254128+brooke2384@users.noreply.github.com>
2024-08-24 14:51:20 +0300

    frontend2

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 const home = () => {
 
@@ -14,6 +15,7 @@ const home = () => {
       <Header/>
       <ExploreMenu category= {category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload />
     </div>
   )
 }
